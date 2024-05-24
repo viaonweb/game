836bd0a6d3d451ddba48cdf6aca9f59da588e990
viaonweb <419499973@qq.com>
2024-05-24 17:08:04 +0800

    update

--- game.js
+++ game.js
@@ -1,15 +1,5 @@
 import uniplay from 'uniplay';
 
-const getCookie = (cname) => {
-    let name = cname + "=";
-    let ca = document.cookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i].trim();
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-}
 if (!uniplay.Utils.getCookie('Authorization')) {
     uniplay.login();
 }
